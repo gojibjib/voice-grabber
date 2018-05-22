f87bafe3ccd0c58e28edd5a580db616b0c0553b2
obitech <obitech@users.noreply.github.com>
2018-05-22 22:24:29 +0200

    using go to download now

---
+++ xeno_grabber/xeno_downloader.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+	"time"
+)
+
+type DL struct {
+	Name  string   `json:"name"`
+	Links []string `json:"links"`
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func downloadFile(query, filepath string) int {
+	var code int
+
+	// Get the data
+	client := http.Client{Timeout: 10 * time.Second}
+	req, err := http.NewRequest("GET", query, nil)
+	checkErr(err)
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Nice crawler for extracting German birds and sounds. See github.com/gojibjib - Thank you)")
+
+	resp, err := client.Do(req)
+	checkErr(err)
+	defer resp.Body.Close()
+
+	// Check status code
+	code = resp.StatusCode
+	if code != http.StatusOK {
+		log.Printf("Bad status: %d", resp.StatusCode)
+		return code
+	}
+
+	// Creating the file
+	out, err := os.Create(filepath)
+	checkErr(err)
+	defer out.Close()
+
+	// Write to file
+	_, err = io.Copy(out, resp.Body)
+	checkErr(err)
+
+	return code
+}
+
+func main() {
+	// Load JSON
+	var dlMap map[string][]string
+	jsonName := "dl_dict.json"
+	filesDir, _ := filepath.Abs("../files")
+
+	jsonFile, err := ioutil.ReadFile(jsonName)
+	checkErr(err)
+
+	err = json.Unmarshal(jsonFile, &dlMap)
+	checkErr(err)
+
+	// URL things
+	url := "https://www.xeno-canto.org"
+	n := len(dlMap)
+	var c int
+
+	for k, v := range dlMap {
+		name := strings.Replace(k, "-", "_", -1)
+
+		fmt.Printf("%d/%d - %s\n", c+1, n, name)
+
+		maxDl := len(v)
+		for idx, id := range v {
+			idFile := strings.Replace(id, "/", "", -1)
+			fName := fmt.Sprintf("%s-%s.mp3", name, idFile)
+			fPath := path.Join(filesDir, name, fName)
+
+			// Download, if file is missing
+			if _, err := os.Stat(fPath); os.IsNotExist(err) {
+				fmt.Printf("%d/%d: %s ", idx+1, maxDl, id)
+				currUrl := fmt.Sprintf("%s%s/download", url, id)
+				downloadFile(currUrl, fPath)
+				time.Sleep(500 * time.Millisecond)
+			}
+		}
+		os.Exit(0)
+		fmt.Println()
+		c++
+	}
+}
--- xeno_grabber/xeno_downloader.py
+++ xeno_grabber/xeno_downloader.py
@@ -4,6 +4,7 @@
 import requests, os, time, pickle
 import pandas as pd
 from bs4 import BeautifulSoup as bs
+from traceback import print_exc
 
 # Get birds
 birds_csv = os.path.abspath("../birds_final.csv")
@@ -11,6 +12,7 @@ df = pd.read_csv(birds_csv, index_col=0, sep='|')
 birds = df['name'].values.tolist()
 birds_folder = [x.replace(" ", "_") for x in birds]
 birds_xeno = [x.replace(" ", "-") for x in birds]
+files_folder = os.path.abspath("../files")
 
 # Initialize requests
 proto = "https://"
@@ -23,7 +25,7 @@ header_dict = {
 dl_dict = {}
 birds_total = len(birds_xeno)
 curr_bird = 1
-# For every bird ..
+For every bird ..
 for species in birds_xeno:
     print("{}/{} - {}".format(curr_bird, birds_total, species))
     
@@ -49,6 +51,45 @@ for species in birds_xeno:
         i += 1
         time.sleep(1)
 
-    dl_dict[species] = ids
+    dl_dict[species.replace("-", "_")] = ids
     curr_bird += 1
 
+# Save as JSON
+with open("dl_dict.json", 'w') as wf:
+    json.dump(dl_dict, wf)
+
+# curr_bird = 1
+# for species, ids in dl_dict.items():
+#     name = species.replace("-", "_")
+#     if name in already:
+#         continue
+    
+#     print("{}/{} - {}".format(curr_bird, birds_total, name))
+#     total_dl = len(ids)
+#     curr_dl = 1
+#     for i in ids:
+#         print("{}/{} - {}".format(curr_dl, total_dl, i))
+#         curr_url = "{}{}{}/download".format(proto, download_url, i)
+#         try:
+#             # timouts: 10s connect, 20s read
+#             r = requests.get(curr_url, timeout=(10,20), headers=header_dict)
+#         except:
+#             print_exc()
+#             continue
+#         if r.status_code != 200:
+#             print("Got status code {} on {}".format(r.status_code, curr_url))
+#             continue
+
+#         # Saving file
+#         fname = "{}-{}.mp3".format(name , i.replace("/", ""))
+#         path = os.path.join(files_folder, name, fname)
+        
+#         try:
+#             with open(path, 'wb') as wf:
+#                 wf.write(r.content)
+#         except:
+#             print_exc()
+
+#         curr_dl += 1
+#         time.sleep(1)
+#     curr_bird += 1
