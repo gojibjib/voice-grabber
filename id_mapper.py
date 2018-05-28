eb07ed6a87d81e8e16d9ab2080a345f5499e89ff
obitech <obitech@users.noreply.github.com>
2018-05-28 19:39:59 +0200

    added script + pickle to map bird names to DB ids

---
+++ id_mapper.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import json
+import os
+import pickle
+
+csv_path = os.path.abspath("./meta/birds.csv")
+df = pd.read_csv(csv_path, index_col=0, sep="|")
+
+bird_id_map = {}
+n = len(df)
+for i in range(n):
+    name = df['name'].iat[i].replace(" ", "_")
+    bird_id_map[name] = i
+
+print(bird_id_map)
+
+pickle_path = os.path.abspath("./meta/bird_id_map.pickle")
+with open(pickle_path, "wb") as wf:
+    pickle.dump(bird_id_map, wf)
